d953bb885089bdb603f0dfdf686c27a342fe1e9e
Chad Yeates <chad.yeates@gmail.com>
2015-01-22 15:13:25 -0600

    calculating greatest product of four adjacent numbers in 20x20 grid

---
+++ Exercises/GreatestProductInGrid/GreatestProductInGridTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Exercises.GreatestProductInGrid
+{
+  [TestFixture]
+  public class GreatestProductInGridTests
+  {
+
+    [Test]
+    public void Should_Calculate_Greatest_Product_In_Grid()
+    {
+      var calculator = new GreatestProductInGridCalculator();
+      int product = calculator.Calculate();
+      Assert.AreEqual(70600674, product);
+    }
+  }
+}
